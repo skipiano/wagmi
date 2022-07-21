473357557e892f595e63e81478d683ac26e3e0a5
Michael Hahn <skipiano@gmail.com>
2022-07-21 14:13:27 +0900

    implement menu bar

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -2,6 +2,9 @@ import { Button, Flex, Input, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase/clientApp";
+import { FIREBASE_ERRORS } from "../../../firebase/errors";
 
 type LoginProps = {};
 
@@ -12,8 +15,13 @@ const Login: React.FC<LoginProps> = () => {
         email: "",
         password: "",
     });
+    const [signInWithEmailAndPassword, user, loading, error] =
+        useSignInWithEmailAndPassword(auth);
 
-    const onSubmit = () => {};
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        signInWithEmailAndPassword(loginForm.email, loginForm.password);
+    };
 
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setLoginForm((prev) => ({
@@ -68,7 +76,21 @@ const Login: React.FC<LoginProps> = () => {
                 }}
                 bg="gray.50"
             />
-            <Button width="100%" height="36px" mt={2} mb={2} type="submit">
+            <Text textAlign="center" color="red" fontSize="10pt">
+                {
+                    FIREBASE_ERRORS[
+                        error?.message as keyof typeof FIREBASE_ERRORS
+                    ]
+                }
+            </Text>
+            <Button
+                width="100%"
+                height="36px"
+                mt={2}
+                mb={2}
+                type="submit"
+                isLoading={loading}
+            >
                 Log In
             </Button>
             <Flex fontSize="9pt" justifyContent="center">
