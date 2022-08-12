474c5a140b8f9e6c32c058325bbf64a1f74313ba
Michael Hahn <skipiano@gmail.com>
2022-08-12 12:20:20 +0900

    finish wallet signin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,7 +8,6 @@ export const createUserDocument = functions.auth.user().onCreate(async (user) =>
         admin.firestore().collection("users").doc(user.uid).set(
             {
                 displayName: user.email.split("@")[0],
-                displayNameShort: user.email.split("@")[0].slice(0, 8) + "...",
                 darkMode: false,
                 isPremium: false,
                 karma: 0,
@@ -17,22 +16,5 @@ export const createUserDocument = functions.auth.user().onCreate(async (user) =>
                 createdAt: admin.firestore.FieldValue.serverTimestamp(),
             }
         );
-    } else {
-        admin.firestore().collection("users").doc(user.uid).set(
-            {
-                displayName: user.uid,
-                displayNameShort: `${user.uid.slice(0, 4)}...${user.uid.slice(user.uid.length-4)}`,
-                darkMode: false,
-                isPremium: false,
-                karma: 0,
-                karmaContent: [],
-                isWeb3: true,
-                claimedWAG: 0,
-                totalWAG: 0,
-                wallets: [user.uid],
-                nameService: "",
-                createdAt: admin.firestore.FieldValue.serverTimestamp(),
-            }
-        )
     }
 });
